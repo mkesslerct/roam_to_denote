6dcd13febb2804fbf45a54cc42988a784a306bc0
Mathieu Kessler <mathieu.kessler@upct.es>
2022-07-28 22:13:14 +0200

    Implement correction of mtime to avoid duplicates
    If mtime of two notes was the same, it lead to a duplicate of the denote id

--- roam_to_denote.py
+++ roam_to_denote.py
@@ -1,20 +1,33 @@
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 import shutil
+import os
 import pathlib
 from pathlib import Path
+import pandas as pd
+
 
 # from https://www.peterbe.com/plog/fastest-python-function-to-slugify-a-string
 import re
 non_url_safe = ['"', '#', '$', '%', '&', '+',
                     ',', '/', ':', ';', '=', '?',
                     '@', '[', '\\', ']', '^', '`',
-                    '{', '|', '}', '~', "'", "<", ">"]
+                    '{', '|', '}', '~', "'", "<", ">", "-"]
 non_url_safe_regex = re.compile(
     r'[{}]'.format(''.join(re.escape(x) for x in non_url_safe)))
 def slugify(text):
     text = non_url_safe_regex.sub('', text).strip()
     text = u'-'.join(re.split(r'\s+', text))
     return text
+
+# https://zditect.com/code/python/how-to-set-file-modification-time-mtime-in-python.html#:~:text=How%20to%20set%20file%20modification%20time%20%28mtime%29%20in,%28atime%2C%20mtime%29%20os.utime%20%28filename%2C%20times%3D%20%28stat.st_atime%2C%20mtime.timestamp%20%28%29%29%29
+def set_file_modification_time(file_path, mtime):
+    """
+    Set the modification time of a given file_path to the given mtime.
+    mtime must be a datetime object.
+    """
+    stat = os.stat(file_path)
+    atime = stat.st_atime
+    os.utime(file_path, times=(atime, mtime.timestamp()))
 
 FROM_NOTES_DIR = Path("../../slip-box/pages")
 TO_NOTES_DIR = Path("../../slip-box/denotes")
@@ -35,7 +48,7 @@ def retrieve_title_filetags(file_path):
 
 def retrieve_denote_date(file_path):
     denote_date = datetime.fromtimestamp(
-        file_path.stat().st_mtime, tz=timezone.utc
+        file_path.stat().st_mtime, #tz=timezone.utc
     ).strftime("%Y%m%dT%H%M%S")
 
     return denote_date
@@ -61,14 +74,39 @@ def orgroam_to_denote_filename(file_path):
     denote_filename = f"{date}--{title}__{keywords}.org"
 
     return denote_filename
-    
+ 
+def correct_mtime(path):
+    ids = pd.Series(name="denote_id", dtype="object")
+    for roam_note in path.glob("*.org"):
+        denote_id = retrieve_denote_date(roam_note)
+        ids.loc[roam_note] = denote_id
+    ids.sort_values(inplace=True)
+    prev = ""
+    for i in ids.index:
+        if ids.loc[i] <= prev:
+            prev_dt = datetime.strptime(prev, "%Y%m%dT%H%M%S")
+            set_file_modification_time(i, prev_dt + timedelta(seconds=1))
+            ids.loc[i] = retrieve_denote_date(i)
+        prev = ids.loc[i]
+
+    return None
+
 def build_org_roam_ids(path):
     # with open("org_roam_ids.csv", "w") as csv_file: 
     notes = dict()
+    denote_ids = dict()
         # csv_file.write("path,id\n")
     for roam_note in path.glob("*.org"):
         or_id = retrieve_org_roam_id(roam_note)
         notes[or_id] = roam_note
+        denote_id = retrieve_denote_date(roam_note)
+        if denote_id in denote_ids.values():
+            pass
+            print(f"denote_id already exists {denote_id}. Current note: {roam_note.name}") 
+
+            raise Exception(f"denote_id already exists {denote_id}. Current note: {roam_note.name}") 
+        else:
+            denote_ids[or_id] = denote_id
         # csv_file.write(f"{roam_note},{or_id}\n")
 
     return notes
@@ -103,9 +141,16 @@ def orgroam_to_denote(file_path, notes_dict):
     
 
 if __name__ == "__main__":    
+    correct_mtime(FROM_NOTES_DIR)
     notes_dict = build_org_roam_ids(FROM_NOTES_DIR)
     for roam_note in FROM_NOTES_DIR.glob("*.org"):
         orgroam_to_denote(roam_note, notes_dict)
+
+
+
+
+
+
 
 
 
